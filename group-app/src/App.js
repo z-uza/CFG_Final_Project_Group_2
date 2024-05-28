30fb228c991208d0504fceb6a860919c02797542
Beth Hopla <bethhopla@gmail.com>
2024-05-28 19:10:19 +0100

    skeleton build for app

--- group-app/src/App.js
+++ group-app/src/App.js
@@ -1,53 +1,39 @@
-import logo from './logo.svg';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
+import Home from './components/Home/Home'
+import DrawerAppBar from './components/Nav/Navigation';
+import Favourites from './components/Favourites/Favourites';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './components/DropDown/DropDown.css';
+import './components/ImageCarousel/ImageCarousel.css'; 
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello from Group 2</h1>
-        <p className='App-Para'> We are:
-          <li>Beth</li>
-          <li>Bronwen</li>
-          <li>Majella</li>
-          <li>Sarah</li>
-          <li>Warda</li>
-          <li>Zuzanna</li>
-        </p>
-        <img src="../CFG_logo.png" className="App-logo" alt="logo" />
-        <h2>Our Hobbies and Interests</h2>
-        
-        <p className='App-Para'> <span class ="App-bolded">Majella: </span>my favourite thing to do is cold water swimming in either lakes or the sea. It doesn't always involve 
-          serious swimming though, sometimes its a more of a chill and a good natter with friends.I do this 
-          all year round and absolutely love it! Why do I do this? Because it feels incredible!!  In fact as you build
-          tolerance it is actually quite addictive.  I try to get to the beach at sunrise before work and it's just the 
-          a beautiful way to start the day.  
-          
-        </p>
+    <BrowserRouter> 
+      <div className="App">
 
-        <p className='App-Para'> <span class ="App-bolded">Zuzanna: </span>My favourite thing to do is backpacking the world, 
-        I’ve been travelling and working remotely for the last two years and hope to continue! 
-        I love immersing myself in different cultures and slowly discovering a new place. 
-        I’m learning Spanish to spend some time in Central and South America. 
-        </p>
+        <DrawerAppBar /> 
 
-        <p className='App-Para'> <span class ="App-bolded">Beth: </span> My favourite thing to do is running. It sounds crazy but I love both the mental and physical challenge. I completed Manchester Marathon a
-        few years ago and I am running the London Marathon in 2025. I am part of my local running club [St. Albans Striders] and also really enjoy the social side of it too.
-        </p>
+        <Routes> 
+        <Route path ="/favourites" element={<Favourites />} />
+        <Route path ="/about" element={<About />} />
+        <Route path ="/contact" element={<Contact />} />
+        <Route path="*" element={<Home />} />
+        </Routes>
 
+      </div>
+    </BrowserRouter> 
 
-
-        <a
-          className="App-link"
-          href="https://github.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Find us on GitHub
-        </a>
-      </header>
-    </div>
   );
 }
 
 export default App;
+
+
+
+
---
+++ group-app/src/components/Nav/Navigation.js
@@ -0,0 +1,117 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
+import { Link, useLocation } from 'react-router-dom';
+import themeparkImage from '../../assets/Logos/themepark.png';
+import './Navigation.css'
+
+const drawerWidth = 240;
+const navItems = ['Favourites', 'About', 'Contact'];
+
+function DrawerAppBar(props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+  const userLocation = useLocation(); // Use useLocation to get the current path
+
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  // Check if user is on home page - if not, add 'Home' pathway to nav bar
+  const modifiedNavItems = userLocation.pathname !== '/' ? ['Home', ...navItems] : navItems;
+
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+      </Typography>
+      <Divider />
+      <List>
+        {modifiedNavItems.map((item) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton component={Link} to={`/${item.toLowerCase()}`} sx={{ textAlign: 'center' }}>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  const container = window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar component="nav" className="nav-bar-custom">
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' }, margin: 0 }}
+          > <img src={themeparkImage} alt="Theme Park" style={{ height: '40px' }} /> 
+
+          </Typography>
+          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+            {modifiedNavItems.map((item) => (
+              <Button key={item} component={Link} to={`/${item.toLowerCase()}`} className="nav-button">
+                {item}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <nav>
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, 
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </nav>
+      <Box component="main" sx={{ p: 3 }}>
+        <Toolbar />
+      </Box>
+    </Box>
+  );
+}
+
+DrawerAppBar.propTypes = {
+  window: PropTypes.func,
+};
+
+export default DrawerAppBar;
+
