9bfdc374ac0dc0d1cd0f2cab379a5fa9e867bc80
z-uza <zuziakowska97@gmail.com>
2024-05-29 11:15:34 +0200

    add to about cards

--- group-app/src/components/About/About.js
+++ group-app/src/components/About/About.js
@@ -1,6 +1,7 @@
 import BethImage from '../../assets/Team Images/Beth.png'
 import MajellaImage from '../../assets/Team Images/Majella.png'
 import BronwenImage from '../../assets/Team Images/Bronwen.png'
+import ZuzaImage from '../../assets/Team Images/Zuza.png'
 
 import TeamCards from './TeamCards';
 import './About.css'
@@ -25,6 +26,24 @@ function About() {
         imgSrc={BronwenImage}
         text="Need to decide what to put here"
         buttonText="github link?" />
+
+        <TeamCards 
+        title="Zuza" 
+        imgSrc={ZuzaImage}
+        text="Need to decide what to put here"
+        buttonText="github link?" />
+
+        <TeamCards 
+        title="Sarah" 
+        imgSrc=""
+        text="Need to decide what to put here"
+        buttonText="github link?" /> 
+
+        <TeamCards 
+        title="Warda" 
+        imgSrc=""
+        text="Need to decide what to put here"
+        buttonText="github link?" /> 
         </div>
     )
 }
