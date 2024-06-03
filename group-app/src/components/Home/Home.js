03371e2bea92cdcd1fd6f6a6edd9360715a0073a
MadgeMom <omahonymajella@gmail.com>
2024-06-03 15:26:05 +0100

    created accessibility page with working drop menu and info displayed. components for access dropdown, access button

--- group-app/src/components/Home/Home.js
+++ group-app/src/components/Home/Home.js
@@ -2,6 +2,7 @@ import DropDown from "../DropDown/DropDown";
 import React, { useState } from 'react';
 // import RideList from "../RidePages/RidesList";
 import ExploreButton from "../ExploreButton/ExploreButton";
+import AccessButton from "../AccessButton/AccessButton";
 import Carousel from '../ImageCarousel/ImageCarousel.js'
 
 import 'bootstrap/dist/css/bootstrap.min.css'; // Ensure this is imported first
@@ -12,12 +13,16 @@ import './Home.css'
 function Home() {
     return (
       <div className="home-container">
-          <h1> [title here] </h1>
+          <h1>Fun Times 4 All</h1>
           <hr /> 
+        <div className="button-container">
+        <AccessButton className="custom-btn" />
+        </div>
         <Carousel />
         <div className="button-container">
           <DropDown className="custom-btn" />
           <ExploreButton className="custom-btn" />
+          
         </div>
       </div>
     );
