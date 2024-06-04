065e89604b42eea501227a1b57adc5d1c9df6fb2
Sarah Pascoe <spascoe46@icloud.com>
2024-06-04 13:43:28 +0100

    API component and debugging issues with App.js and Ride.js

---
+++ group-app/src/components/APIComponent/APIComponent.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+function WaitTimePage() {
+  const { id } = useParams(); // Get the parkID parameter from the URL
+  const [waitTime, setWaitTime] = useState(null);
+
+  useEffect(() => {
+    // Fetch wait time for the selected theme park
+    fetchWaitTime(id);
+  }, [id]);
+
+  const fetchWaitTime = async (id) => {
+    try {
+      const response = await fetch(`https://queue-times.com/parks/${id}/queue_times.json`);
+      if (!response.ok) {
+        throw new Error('Failed to fetch wait time');
+      }
+      const data = await response.json();
+      setWaitTime(data);
+    } catch (error) {
+      console.error('Error fetching wait time:', error.message);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Wait Time for {id}</h2>
+      {waitTime ? (
+        <p>Current ride wait times: {waitTime.wait_time} </p>
+      ) : (
+        <p>Loading wait time...</p>
+      )}
+    </div>
+  );
+}
+
+export default WaitTimePage;
