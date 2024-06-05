82359f90819a530197611498c7551893e43c0467
z-uza <zuziakowska97@gmail.com>
2024-06-05 18:39:05 +0100

    about desc updates

--- group-app/src/components/About/About.js
+++ group-app/src/components/About/About.js
@@ -3,7 +3,7 @@ import BethImage from '../../assets/Team Images/Beth.png';
 import MajellaImage from '../../assets/Team Images/Majella.png';
 import BronwenImage from '../../assets/Team Images/Bronwen.png';
 import ZuzaImage from '../../assets/Team Images/Zuza.png';
-
+import WardaImage from '../../assets/Team Images/Warda.jpg';
 import TeamCards from './TeamCards';
 import './About.css';
 
@@ -13,7 +13,7 @@ function About() {
       <TeamCards 
         title="Beth" 
         imgSrc={BethImage}
-        text="Need to decide what to put here"
+        text="My favourite thing to do is running. It sounds crazy but I love both the mental and physical challenge."
         buttonText="GitHub Link"
         link="https://github.com/bhpla9" 
       />
@@ -21,7 +21,7 @@ function About() {
       <TeamCards 
         title="Majella" 
         imgSrc={MajellaImage}
-        text="Need to decide what to put here"
+        text="My favourite thing to do is cold water swimming in either lakes or the sea."
         buttonText="GitHub Link"
         link="https://github.com/MadgeMom" 
       />
@@ -29,7 +29,7 @@ function About() {
       <TeamCards 
         title="Bronwen" 
         imgSrc={BronwenImage}
-        text="Need to decide what to put here"
+        text="My favourite thing to do is play volleyball and skydiving."
         buttonText="GitHub Link"
         link="https://github.com/brongrainger" 
       />
@@ -37,9 +37,27 @@ function About() {
       <TeamCards 
         title="Zuza" 
         imgSrc={ZuzaImage}
-        text="Need to decide what to put here"
+        text="I’ve been travelling and working remotely for the last two years and hope to continue!"
         buttonText="GitHub Link"
         link="https://github.com/z-uza" 
+      />
+
+
+    <TeamCards 
+        title="Warda" 
+        imgSrc={WardaImage}
+        text="My favourite thing to do is workout, and I'm an avid gym-goer."
+        buttonText="GitHub Link"
+        link="https://github.com/wardanot" 
+      />
+
+
+    <TeamCards 
+        title="Sarah" 
+        imgSrc={ZuzaImage}
+        text="My favourite hobby is hiking. I love being in nature and enjoying the beauty of the outdoors"
+        buttonText="GitHub Link"
+        link="Sarah Pascoe" 
       />
     </div>
   );
